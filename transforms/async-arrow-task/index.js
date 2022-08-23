0432d1c16b4792c0c0be8ed7d4e93825853433f0
machty <machty@gmail.com>
2022-08-22 21:48:30 -0400

    cleanup

--- transforms/async-arrow-task/index.js
+++ transforms/async-arrow-task/index.js
@@ -6,44 +6,43 @@ module.exports = function transformer(file, api) {
   const options = getOptions();
   const root = j(file.source);
 
-  return root
-    .find(j.ClassMethod)
-    .forEach((p) => {
-      const jp = j(p);
-      const decorators = jp.find('Decorator', { expression: { name: 'task', type: 'Identifier' } });
-      if (decorators.length > 0) {
-        // Transform
-        // - ClassMethod:   @task *a(uid: string) { yield timeout(1); }
-        // + ClassProperty: a = task(this, async (uid: string) => { await timeout(1); });
-
-        const asyncArrowFn = j.arrowFunctionExpression(p.value.params, p.value.body, true);
-        asyncArrowFn.async = true;
-
-        let newClassProperty = j.classProperty(
-          j.identifier(p.node.key.name),
-          j.callExpression(j.identifier('task'), [j.thisExpression(), asyncArrowFn])
+  function convertYieldsToAwaits(q) {
+    q.find(j.YieldExpression).forEach((path) => {
+      const { argument, comments } = path.value;
+      if (argument && argument.type === 'ConditionalExpression') {
+        const { test, consequent, alternate } = argument;
+        const stmt = j.conditionalExpression(
+          test,
+          j.awaitExpression(consequent),
+          j.awaitExpression(alternate)
         );
-        const q = jp.replaceWith(newClassProperty);
-
-        q.find(j.YieldExpression).forEach((path) => {
-          const { argument, comments } = path.value;
-          if (argument && argument.type === 'ConditionalExpression') {
-            const { test, consequent, alternate } = argument;
-            const stmt = j.conditionalExpression(
-              test,
-              j.awaitExpression(consequent),
-              j.awaitExpression(alternate)
-            );
-            j(path).replaceWith(stmt);
-          } else {
-            const awaitExp = j.awaitExpression(argument);
-            awaitExp.comments = comments;
-            j(path).replaceWith(awaitExp);
-          }
-        });
+        j(path).replaceWith(stmt);
+      } else {
+        const awaitExp = j.awaitExpression(argument);
+        awaitExp.comments = comments;
+        j(path).replaceWith(awaitExp);
       }
-    })
-    .toSource();
+    });
+  }
+
+  root.find(j.ClassMethod, { decorators: [{ expression: { name: 'task' } }] }).forEach((p) => {
+    const jp = j(p);
+    // Transform
+    // - ClassMethod:   @task *a(uid: string) { yield timeout(1); }
+    // + ClassProperty: a = task(this, async (uid: string) => { await timeout(1); });
+
+    const asyncArrowFn = j.arrowFunctionExpression(p.value.params, p.value.body, true);
+    asyncArrowFn.async = true;
+
+    let newClassProperty = j.classProperty(
+      j.identifier(p.node.key.name),
+      j.callExpression(j.identifier('task'), [j.thisExpression(), asyncArrowFn])
+    );
+    const q = jp.replaceWith(newClassProperty);
+    convertYieldsToAwaits(q);
+  });
+
+  return root.toSource();
 };
 
 module.exports.type = 'js';
